d1aedb2d65ea8441a2252cfd24a450011bd01b78
jsalvador-zsh <juanstdev@gmail.com>
2025-09-07 10:56:16 -0500

    [ADD] peruanita_bg_layout: versión inicial estable para producción

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Layout Background Peruanita",
+    "author": "Juan Salvador",
+    "description": "Módulo para agregar un encabezado y pie de página a los reportes",
+    "depends": [
+        "base",
+        "base_setup",
+        "web"
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    "category": "uncategorized",
+    'version': '2.0',
+    "data": [
+        'reports/paperformat.xml',
+        'reports/peruanita_layout_background.xml'
+    ],
+}
